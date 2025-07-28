6232f19462d80b3609763009d2a74f649d29b8e8
ghulam-mujtaba5 <ghulammujtaba1005@gmail.com>
2025-07-29 02:53:43 +0500

    .

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -13,12 +13,12 @@ export const brands: Brand[] = [
   {
     id: '2',
     name: 'Alkaram Studio',
-    logo: 'https://www.alkaramstudio.com/static/version1719393355/frontend/Alkaram/default/en_US/images/logo.svg',
+    logo: 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTUwIiBoZWlnaHQ9IjYwIiB2aWV3Qm94PSIwIDAgMTUwIDYwIiBmaWxsPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxyZWN0IHdpZHRoPSIxNTAiIGhlaWdodD0iNjAiIGZpbGw9IiNmYWVmY2YiLz48dGV4dCB4PSI3NSIgeT0iMzUiIHRleHQtYW5jaG9yPSJtaWRkbGUiIGZpbGw9IiMzNzQxNTEiIGZvbnQtZmFtaWx5PSJBcmlhbCwgc2Fucy1zZXJpZiIgZm9udC1zaXplPSIxNiIgZm9udC13ZWlnaHQ9ImJvbGQiPkFsa2FyYW08L3RleHQ+PC9zdmc+',
   },
   {
     id: '3',
     name: 'Khaadi',
-    logo: 'https://pk.khaadi.com/static/version1719472242/frontend/Khaadi/default/en_US/images/logo.svg',
+    logo: 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTUwIiBoZWlnaHQ9IjYwIiB2aWV3Qm94PSIwIDAgMTUwIDYwIiBmaWxsPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxyZWN0IHdpZHRoPSIxNTAiIGhlaWdodD0iNjAiIGZpbGw9IiNmZmZlYmMiLz48dGV4dCB4PSI3NSIgeT0iMzUiIHRleHQtYW5jaG9yPSJtaWRkbGUiIGZpbGw9IiMzNzQxNTEiIGZvbnQtZmFtaWx5PSJBcmlhbCwgc2Fucy1zZXJpZiIgZm9udC1zaXplPSIxNiIgZm9udC13ZWlnaHQ9ImJvbGQiPktIYWFkaTwvdGV4dD48L3N2Zz4=',
   },
   {
     id: '4',
@@ -29,6 +29,6 @@ export const brands: Brand[] = [
   {
     id: '6',
     name: 'Sana Safinaz',
-    logo: 'https://www.sanasafinaz.com/static/version1719472898/frontend/Codazon/unlimited_rtl/en_US/images/logo.svg',
+    logo: 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTUwIiBoZWlnaHQ9IjYwIiB2aWV3Qm94PSIwIDAgMTUwIDYwIiBmaWxsPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxyZWN0IHdpZHRoPSIxNTAiIGhlaWdodD0iNjAiIGZpbGw9IiNmZmVmZmYiLz48dGV4dCB4PSI3NSIgeT0iMzUiIHRleHQtYW5jaG9yPSJtaWRkbGUiIGZpbGw9IiMzNzQxNTEiIGZvbnQtZmFtaWx5PSJBcmlhbCwgc2Fucy1zZXJpZiIgZm9udC1zaXplPSIxNiIgZm9udC13ZWlnaHQ9ImJvbGQiPlNhbmEgU2FmaW5heiA8L3RleHQ+PC9zdmc+',
   },
 ];
